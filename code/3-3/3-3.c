b4305dea32830d02245cb8268eb4b12b74f51bbd
LuminApo-k <beomseomoon@gmail.com>
2026-03-27 10:28:36 +0900

    Update 3-3.c

--- code/3-3/3-3.c
+++ code/3-3/3-3.c
@@ -2,10 +2,10 @@
 
 int main()
 {
-    printf("sezeof(int*) = %zu\n", sizeof(int*));
-    printf("sezeof(double*) = %zu\n", sizeof(double*));
-    printf("sezeof(char*) = %zu\n", sizeof(char*));
-    printf("sezeof(void*) = %zu\n", sizeof(void*));
+    printf("sizeof(int*) = %zu\n", sizeof(int*));
+    printf("sizeof(double*) = %zu\n", sizeof(double*));
+    printf("sizeof(char*) = %zu\n", sizeof(char*));
+    printf("sizeof(void*) = %zu\n", sizeof(void*));
     
     return 0;
 }
