daec8e6253a3dfa84832fb2c2e83779d48413ae7
LuminApo-k <beomseomoon@gmail.com>
2026-04-02 22:04:01 +0900

    Update 4-3.c
    Updated address printing logic to show byte difference.

--- code/4-3/4-3.c
+++ code/4-3/4-3.c
@@ -5,12 +5,14 @@ int main() {
     int n = sizeof(arr) / sizeof(arr[0]);
 
     for (int i = 0; i < n - 1; i++) {
-        int* addr = &arr[i];
-        int* addr_next = &arr[i + 1];
+        int *addr = &arr[i];
+        int *addr_next = &arr[i + 1];
 
-        printf("address of arr[%d]: %p\n", i, (void*)addr);
-        printf("address of arr[%d]: %p\n", i + 1, (void*)addr_next);
-        printf("diff: %td\n\n", addr_next - addr);
+        printf("address of arr[%d]: %p\n", i, (void *)addr);
+        printf("address of arr[%d+1]: %p\n", i, (void *)addr_next);
+        printf("&(arr[%d]) - &(arr[%d+1]) = %ld\n",
+               i, i + 1, (long)((char *)addr_next - (char *)addr));
+        printf("\n");
     }
 
     return 0;
