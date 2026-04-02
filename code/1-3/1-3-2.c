0b8b9d8ef7c64c5c8a8b77b2bead2c5a0172e26e
LuminApo-k <beomseomoon@gmail.com>
2026-04-02 22:06:19 +0900

    Update 1-3-2.c
    Updated logger_global format and adjusted loop count in task2.

--- code/1-3/1-3-2.c
+++ code/1-3/1-3-2.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
+#include "util.h"
 
 void logger_global(char* message) {
     static int counter = 0;
-    counter++;
-    printf("call %d: %s\n", counter, message);
+    printf("log: %2d: %s\n", counter++, message);
 }
 
 void task2() {
-    for (int i = 0; i < 5; i++) {
+    for (int i = 0; i < 4; i++) {
         logger_static_inline("task2");
         logger_global("task2");
     }
