93cd820ab97076bddba75fd9815eeb87415e5eeb
LuminApo-k <beomseomoon@gmail.com>
2026-04-02 22:01:10 +0900

    Update 2-4.c

--- code/2-4/2-4.c
+++ code/2-4/2-4.c
@@ -2,17 +2,18 @@
 #include <ctype.h>
 
 int main() {
-    char chars[] = { 'A', 'z', '5', '!', '\0' };
+    char chars[] = { 'A', 'z', '5', '!' };
     int n = sizeof(chars) / sizeof(chars[0]);
 
     for (int i = 0; i < n; i++) {
         char ch = chars[i];
+
         printf("%c - alpha:%d digit:%d upper:%c lower:%c\n",
                ch,
-               isalpha((unsigned char)ch),
-               isdigit((unsigned char)ch),
-               isalpha((unsigned char)ch) ? toupper((unsigned char)ch) : ' ',
-               isalpha((unsigned char)ch) ? tolower((unsigned char)ch) : ' ');
+               isalpha((unsigned char)ch) ? 1 : 0,
+               isdigit((unsigned char)ch) ? 1 : 0,
+               toupper((unsigned char)ch),
+               tolower((unsigned char)ch));
     }
 
     return 0;
